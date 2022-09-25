72baa18588335050b48aa3fb3df91fce506f654e
fcoseoanedev <fco.seoane.dev@gmail.com>
2022-09-25 12:49:28 +0200

    09_07_ejercicio_añadir_atributos

---
+++ scripts/09_07_ejercicio_añadir_atributos.js
@@ -0,0 +1,18 @@
+
+let persona = {
+  Nombre: "Francisco", 
+  Edad: 47
+};
+
+
+console.log("Valores iniciales Persona:");
+console.log(persona);
+
+persona.Altura = 1.79;
+
+console.log("Añadiendo propiedad Altura:");
+
+console.log(persona);
+
+
+
